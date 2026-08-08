51db80ae7578bc203e9909ff4d58a4fb4cd0520a
DeadPackets <youssef@ctf.ae>
2026-08-08 18:12:59 +0000

    bisect: parameterise only SQL Injection challenges 5, 6 and 7

--- src/main/java/servlets/module/challenge/SqlInjection5CouponCheck.java
+++ src/main/java/servlets/module/challenge/SqlInjection5CouponCheck.java
@@ -75,13 +75,12 @@ public class SqlInjection5CouponCheck extends HttpServlet {
         htmlOutput = new String("");
         Connection conn =
             Database.getChallengeConnection(applicationRoot, "SqlInjectionChallenge5ShopCoupon");
-        log.debug("Looking for Coupons Insecurely");
+        log.debug("Looking for Coupons");
         PreparedStatement prepstmt =
             conn.prepareStatement(
                 "SELECT itemId, perCentOff, itemName FROM coupons JOIN items USING (itemId) WHERE"
-                    + " couponCode = '"
-                    + couponCode
-                    + "';");
+                    + " couponCode = ?;");
+        prepstmt.setString(1, couponCode);
         ResultSet coupons = prepstmt.executeQuery();
         try {
           if (coupons.next()) {
