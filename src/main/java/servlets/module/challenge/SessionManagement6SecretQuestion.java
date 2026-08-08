2b4e96e1d4a8d9772811babbf79c62cf8e25cab5
kabinet01 <chuazd01@gmail.com>
2026-08-08 06:16:48 +0000

    restore: full patch set (ends probe B)

--- src/main/java/servlets/module/challenge/SessionManagement6SecretQuestion.java
+++ src/main/java/servlets/module/challenge/SessionManagement6SecretQuestion.java
@@ -156,8 +156,8 @@ public class SessionManagement6SecretQuestion extends HttpServlet {
   }
 
   /**
-   * A user submits an email address to get that user's Secret QUestion. This is vulnerable to SQL
-   * injection
+   * A user submits an email address to get that user's Secret Question. The query is parameterised
+   * so the submitted email cannot alter the executed SQL.
    *
    * @param subEmail Sub schema user email to search DB with
    */
@@ -225,10 +225,8 @@ public class SessionManagement6SecretQuestion extends HttpServlet {
                         ApplicationRoot, "BrokenAuthAndSessMangChalSix");
                 log.debug("Getting Secret Question");
                 PreparedStatement callstmt =
-                    conn.prepareStatement(
-                        "SELECT secretQuestion FROM users WHERE userAddress = \""
-                            + subEmail
-                            + "\"");
+                    conn.prepareStatement("SELECT secretQuestion FROM users WHERE userAddress = ?");
+                callstmt.setString(1, subEmail);
                 ResultSet rs = callstmt.executeQuery();
                 if (rs.next()) {
                   log.debug("'Valid' User Detected");
