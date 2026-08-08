e18f8ed31c785bac243752292d7d4585ab6f00e6
Gabriel Seet <gabeseet@gmail.com>
2026-08-08 15:31:20 -0700

    fix: prevent URL access user enumeration
    Signed-off-by: Gabriel Seet <gabeseet@gmail.com>

--- src/main/java/servlets/module/challenge/UrlAccess3UserList.java
+++ src/main/java/servlets/module/challenge/UrlAccess3UserList.java
@@ -9,12 +9,10 @@ import java.sql.ResultSet;
 import java.util.Locale;
 import java.util.ResourceBundle;
 import javax.servlet.ServletException;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import org.apache.commons.codec.binary.Base64;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.owasp.encoder.Encode;
@@ -70,29 +68,13 @@ public class UrlAccess3UserList extends HttpServlet {
       String htmlOutput = new String();
 
       try {
-        Cookie userCookies[] = request.getCookies();
-        int i = 0;
-        Cookie theCookie = null;
-        for (i = 0; i < userCookies.length; i++) {
-          if (userCookies[i].getName().compareTo("currentPerson") == 0) {
-            theCookie = userCookies[i];
-            break; // End Loop, because we found the token
-          }
-        }
-        String currentUser = new String("aGuest");
-        if (theCookie != null) {
-          log.debug("Cookie value: " + theCookie.getValue());
-          byte[] decodedCookieBytes = Base64.decodeBase64(theCookie.getValue());
-          String decodedCookie = new String(decodedCookieBytes, "UTF-8");
-          log.debug("Decoded Cookie: " + decodedCookie);
-          currentUser = decodedCookie;
-        }
+        // This sub-application only exposes the guest view. A client-supplied cookie cannot
+        // identify a different user or authorize disclosure of the administrative user list.
+        String currentUser = "aGuest";
         String ApplicationRoot = getServletContext().getRealPath("");
         Connection conn = Database.getChallengeConnection(ApplicationRoot, "UrlAccessThree");
         PreparedStatement callstmt;
-        callstmt =
-            conn.prepareStatement(
-                "SELECT userName FROM users WHERE userRole = 'admin' OR userName = ?;");
+        callstmt = conn.prepareStatement("SELECT userName FROM users WHERE userName = ?;");
         callstmt.setString(1, currentUser);
         log.debug("Getting User List");
         htmlOutput = new String();
