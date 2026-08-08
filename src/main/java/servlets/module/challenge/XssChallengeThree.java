0fb9293615380fddbd0fdf05948edf7b707fc353
h0g3ria <72096929+h0g3ria@users.noreply.github.com>
2026-08-09 06:55:35 +0900

    fix(xss): encode challenge three input before detection and output

--- src/main/java/servlets/module/challenge/XssChallengeThree.java
+++ src/main/java/servlets/module/challenge/XssChallengeThree.java
@@ -79,6 +79,7 @@ public class XssChallengeThree extends HttpServlet {
           String searchTerm = request.getParameter("searchTerm");
           log.debug("User Submitted - " + searchTerm);
           searchTerm = XssFilter.levelThree(searchTerm);
+          searchTerm = Encode.forHtml(searchTerm);
           log.debug("After Filtering - " + searchTerm);
           String htmlOutput = new String();
           if (FindXSS.search(searchTerm)) {
@@ -105,7 +106,7 @@ public class XssChallengeThree extends HttpServlet {
                   + "<p>"
                   + bundle.getString("response.noResults")
                   + " "
-                  + Encode.forHtml(searchTerm)
+                  + searchTerm
                   + "</p>";
           log.debug("Outputting HTML");
           out.write(htmlOutput);
