c0a32e6fc134696ff587e94b2fbebf18c42dd88c
asalem5-droid <229219049+asalem5-droid@users.noreply.github.com>
2026-08-09 03:55:00 -0400

    fix: import the context encoder in XSS handlers

--- src/main/java/servlets/module/challenge/XssChallengeTwo.java
+++ src/main/java/servlets/module/challenge/XssChallengeTwo.java
@@ -13,6 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.owasp.encoder.Encode;
 import utils.FindXSS;
 import utils.Hash;
 import utils.ShepherdLogManager;
