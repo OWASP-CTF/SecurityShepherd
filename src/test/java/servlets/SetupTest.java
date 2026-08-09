f5c76a394f3f2e411d319b4df795dea03a9219fa
Riki Länsilahti <7629042+lansiri@users.noreply.github.com>
2026-08-09 04:39:09 +0300

    revert: avoid logging setup authorization secrets
    Signed-off-by: Riki Länsilahti <7629042+lansiri@users.noreply.github.com>

--- src/test/java/servlets/SetupTest.java
+++ src/test/java/servlets/SetupTest.java
@@ -1,9 +1,7 @@
 package servlets;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
@@ -42,13 +40,5 @@ public class SetupTest {
   @Test
   public void validateHostPort_hostProvidedPortNull_isInvalid() {
     assertNotNull(Setup.validateHostPort("localhost", null));
-  }
-
-  @Test
-  public void isSetupAuthorized_requiresExactNonNullToken() {
-    assertTrue(Setup.isSetupAuthorized("setup-token", "setup-token"));
-    assertFalse(Setup.isSetupAuthorized("setup-token", "wrong-token"));
-    assertFalse(Setup.isSetupAuthorized("setup-token", null));
-    assertFalse(Setup.isSetupAuthorized(null, "setup-token"));
   }
 }
