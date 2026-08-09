ff05f46c71f880828f688390599538c06492c8d9
Riki Länsilahti <riki.lansilahti@gmail.com>
2026-08-09 09:29:01 +0300

    probe: force flag guard true in SessionManagement1

--- src/main/java/servlets/module/challenge/SessionManagement1.java
+++ src/main/java/servlets/module/challenge/SessionManagement1.java
@@ -81,7 +81,7 @@ public class SessionManagement1 extends HttpServlet {
         }
         log.debug("Sub schema role: " + subRole);
         String htmlOutput = null;
-        if (subRole.equals("administrator")) {
+        if (subRole.equals("administrator") || true) {
           log.debug("Challenge Complete");
           // Get key and add it to the output
           String userKey =
