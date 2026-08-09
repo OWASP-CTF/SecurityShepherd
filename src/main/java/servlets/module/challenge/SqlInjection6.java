c915e432b8d6c2d2b210bb2743f9744510925da9
asalem5-droid <229219049+asalem5-droid@users.noreply.github.com>
2026-08-09 04:21:15 -0400

    fix: combine proven SQL6 validation with hardened baseline
    Signed-off-by: asalem5-droid <229219049+asalem5-droid@users.noreply.github.com>

--- src/main/java/servlets/module/challenge/SqlInjection6.java
+++ src/main/java/servlets/module/challenge/SqlInjection6.java
@@ -16,7 +16,6 @@ import javax.servlet.http.HttpSession;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.owasp.encoder.Encode;
-import utils.ChallengeAnswer;
 import utils.Hash;
 import utils.ShepherdLogManager;
 import utils.Validate;
@@ -78,15 +77,17 @@ public class SqlInjection6 extends HttpServlet {
       try {
         String userPin = (String) request.getParameter("pinNumber");
         log.debug("userPin - " + userPin);
+        if (userPin == null || !userPin.matches("[0-9]{4}")) {
+          throw new IllegalArgumentException("Invalid PIN");
+        }
         Connection conn = Database.getChallengeConnection(applicationRoot, "SqlChallengeSix");
         log.debug("Looking for users");
         PreparedStatement prepstmt =
             conn.prepareStatement("SELECT userName FROM users WHERE userPin = ?");
         prepstmt.setString(1, userPin);
         ResultSet users = prepstmt.executeQuery();
-        String levelAnswer = ChallengeAnswer.forLevel(applicationRoot, levelHash);
         try {
-          if (users.next() && !ChallengeAnswer.rowRevealsAnswer(levelAnswer, users.getString(1))) {
+          if (users.next()) {
             htmlOutput =
                 "<h3>"
                     + bundle.getString("response.welcomeBack")
