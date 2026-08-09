6971ac95c5a68e9395a9420599111c7b7b9ec30d
Riki Länsilahti <7629042+lansiri@users.noreply.github.com>
2026-08-09 08:56:08 +0300

    style: format feedback encoding test
    Signed-off-by: Riki Länsilahti <7629042+lansiri@users.noreply.github.com>

--- src/test/java/servlets/module/SolutionSubmitEncodingTest.java
+++ src/test/java/servlets/module/SolutionSubmitEncodingTest.java
@@ -15,8 +15,7 @@ class SolutionSubmitEncodingTest {
             "generateFeedbackForm", String.class, String.class, String.class);
     generateFeedbackForm.setAccessible(true);
 
-    String form =
-        (String) generateFeedbackForm.invoke(null, injection, injection, injection);
+    String form = (String) generateFeedbackForm.invoke(null, injection, injection, injection);
 
     assertFalse(form.contains(injection));
     assertFalse(form.contains("</script><script>alert(2)</script>"));
