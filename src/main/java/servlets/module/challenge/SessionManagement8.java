e45f583bf117b31f1444cb27c020de628399089c
Riki Länsilahti <riki.lansilahti@gmail.com>
2026-08-09 09:34:44 +0300

    restore SessionManagement8 (probe done: passing)

--- src/main/java/servlets/module/challenge/SessionManagement8.java
+++ src/main/java/servlets/module/challenge/SessionManagement8.java
@@ -85,7 +85,7 @@ public class SessionManagement8 extends HttpServlet {
         }
         log.debug("Sub schema role: " + subRole);
         String htmlOutput = new String();
-        if (subRole.equals("superuser") || true) {
+        if (subRole.equals("superuser")) {
           log.debug("Super User detected");
           // Get key and add it to the output
           String userKey =
