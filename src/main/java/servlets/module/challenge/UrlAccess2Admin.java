14efcf3d875d79902d930a85745d422f2e787d3c
asalem5-droid <asalem5@u.rochester.edu>
2026-08-08 23:58:40 -0400

    Require challenge-scoped admin capabilities

--- src/main/java/servlets/module/challenge/UrlAccess2Admin.java
+++ src/main/java/servlets/module/challenge/UrlAccess2Admin.java
@@ -58,7 +58,9 @@ public class UrlAccess2Admin extends HttpServlet {
     ResourceBundle bundle =
         ResourceBundle.getBundle("i18n.servlets.challenges.urlAccess.urlAccess2", locale);
 
-    if (Validate.validateSession(ses)) {
+    boolean hasChallengeGrant =
+        Boolean.TRUE.equals(ses.getAttribute("urlAccess2AdminGrant"));
+    if (Validate.validateSession(ses) && hasChallengeGrant) {
       ShepherdLogManager.setRequestIp(
           request.getRemoteAddr(),
           request.getHeader("X-Forwarded-For"),
@@ -111,7 +113,8 @@ public class UrlAccess2Admin extends HttpServlet {
       log.debug("Outputting HTML");
       out.write(htmlOutput);
     } else {
-      log.error(levelName + " servlet accessed with no session");
+      response.sendError(HttpServletResponse.SC_FORBIDDEN);
+      log.error(levelName + " servlet accessed without its challenge administrator grant");
     }
   }
 }
