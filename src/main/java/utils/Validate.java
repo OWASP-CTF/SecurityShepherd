e0b40628b26dc258e584e13df9a45755cfee12c5
Riki Länsilahti <7629042+lansiri@users.noreply.github.com>
2026-08-09 09:18:23 +0300

    Revert "Invalidate sessions after admin role revocation"
    This reverts commit 32b05195fb6ff72d938e25af3039181e7a20be8b.
    
    Signed-off-by: Riki Länsilahti <7629042+lansiri@users.noreply.github.com>

--- src/main/java/utils/Validate.java
+++ src/main/java/utils/Validate.java
@@ -241,9 +241,7 @@ public class Validate {
             // log.debug("Session holder is " + userName);
             String role = (String) ses.getAttribute("userRole");
             result = (role.compareTo("admin") == 0);
-            if (result && invalidateKickedSession(ses, userName)) {
-              result = false;
-            } else if (!result) {
+            if (!result) {
               log.fatal(
                   "User " + userName + " Attempting Admin functions! (CSRF Tokens Not Checked)");
             }
@@ -283,9 +281,7 @@ public class Validate {
             // log.debug("Session holder is " + userName);
             String role = (String) ses.getAttribute("userRole");
             result = (role.compareTo("admin") == 0);
-            if (result && invalidateKickedSession(ses, userName)) {
-              result = false;
-            } else if (!result) {
+            if (!result) {
               // Check CSRF Tokens of User to ensure they are not being CSRF'd into causing
               // Unauthorised Access Alert
               boolean validCsrfTokens = validateTokens(cookieToken, requestToken);
@@ -309,17 +305,6 @@ public class Validate {
       }
     }
     return result;
-  }
-
-  private static boolean invalidateKickedSession(HttpSession ses, String userName) {
-    if (!UserKicker.shouldKickUser(userName)) {
-      return false;
-    }
-
-    log.debug(userName + " has been revoked. Invalidating administrator session");
-    ses.invalidate();
-    UserKicker.removeFromKicklist(userName);
-    return true;
   }
 
   /**
