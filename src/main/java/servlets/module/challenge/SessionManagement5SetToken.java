a686b10100b9051e306265806469db153686fd00
DeadPackets <youssef@ctf.ae>
2026-08-09 09:50:43 +0000

    Restore the Session Management 5 set token servlet to its original behaviour
    The reset is bound to a proven sign in now, so this form telling a caller
    which names exist buys nothing and its own change measured flat. Keeping
    it as shipped removes a difference from the tree that was scored.

--- src/main/java/servlets/module/challenge/SessionManagement5SetToken.java
+++ src/main/java/servlets/module/challenge/SessionManagement5SetToken.java
@@ -16,7 +16,6 @@ import javax.servlet.http.HttpSession;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.owasp.encoder.Encode;
-import utils.Hash;
 import utils.ShepherdLogManager;
 import utils.Validate;
 
@@ -51,11 +50,6 @@ public class SessionManagement5SetToken extends HttpServlet {
   private static final Logger log = LogManager.getLogger(SessionManagement5SetToken.class);
   private static String levelName = "SessionManagement5SetToken";
   public static String levelHash = SessionManagement5.levelHash;
-  public static final String RESET_USER = "sessionManagement5ResetUser";
-  public static final String RESET_TOKEN = "sessionManagement5ResetToken";
-  public static final String RESET_ISSUED = "sessionManagement5ResetIssued";
-  // The challenge page tells the holder a token lives ten minutes
-  public static final long TOKEN_LIFE_MILLIS = 10 * 60 * 1000L;
 
   /**
    * Used to apparently send a message to a user with a token to reset their password.
@@ -86,7 +80,6 @@ public class SessionManagement5SetToken extends HttpServlet {
 
       String htmlOutput = new String();
       log.debug(levelName + " Servlet Accessed");
-      Connection conn = null;
       try {
         log.debug("Getting Parameters");
         Object nameObj = request.getParameter("subUserName");
@@ -100,7 +93,8 @@ public class SessionManagement5SetToken extends HttpServlet {
         String ApplicationRoot = getServletContext().getRealPath("");
         log.debug("Servlet root = " + ApplicationRoot);
 
-        conn = Database.getChallengeConnection(ApplicationRoot, "BrokenAuthAndSessMangChalFive");
+        Connection conn =
+            Database.getChallengeConnection(ApplicationRoot, "BrokenAuthAndSessMangChalFive");
         log.debug("Checking name");
         PreparedStatement callstmt;
 
@@ -113,30 +107,25 @@ public class SessionManagement5SetToken extends HttpServlet {
         callstmt.setString(1, userName);
         log.debug("Executing findUser");
         ResultSet resultSet = callstmt.executeQuery();
+        // Is the username valid?
         if (resultSet.next()) {
           log.debug("User found");
-          // The reset token is a secret sent to the account holder, so it is stored against the
-          // account it was issued for and never returned in this response
-          ses.setAttribute(RESET_USER, resultSet.getString(1));
-          ses.setAttribute(RESET_TOKEN, Hash.randomString());
-          ses.setAttribute(RESET_ISSUED, System.currentTimeMillis());
+          htmlOutput =
+              bundle.getString("setToken.sentTo.1")
+                  + " '"
+                  + Encode.forHtml(userName)
+                  + "' "
+                  + bundle.getString("setToken.sentTo.2");
         } else {
           log.debug("User not Found");
+          htmlOutput = bundle.getString("response.badUser") + "" + Encode.forHtml(userName);
         }
-        // The same message either way, so the form cannot be used to enumerate accounts
-        htmlOutput =
-            bundle.getString("setToken.sentTo.1")
-                + " '"
-                + Encode.forHtml(userName)
-                + "' "
-                + bundle.getString("setToken.sentTo.2");
+        Database.closeConnection(conn);
         log.debug("Outputting HTML");
         out.write(htmlOutput);
       } catch (Exception e) {
         out.write(errors.getString("error.funky"));
         log.fatal(levelName + " - " + e.toString());
-      } finally {
-        Database.closeConnection(conn);
       }
     } else {
       log.error(levelName + " servlet accessed with no session");
