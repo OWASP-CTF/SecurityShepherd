7d9cbb91a46348784795ddd0f0791760d4b5705c
Riki Länsilahti <7629042+lansiri@users.noreply.github.com>
2026-08-09 10:03:23 +0300

    Revert zero-gain scoreboard session gate
    Signed-off-by: Riki Länsilahti <7629042+lansiri@users.noreply.github.com>

--- src/main/java/servlets/api/Scoreboard.java
+++ src/main/java/servlets/api/Scoreboard.java
@@ -9,7 +9,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import utils.ScoreboardStatus;
-import utils.Validate;
 
 @WebServlet("/api/scoreboard")
 public class Scoreboard extends HttpServlet {
@@ -22,11 +21,7 @@ public class Scoreboard extends HttpServlet {
     PrintWriter out = response.getWriter();
     out.print(getServletInfo());
     HttpSession ses = request.getSession(true);
-    boolean canSeeScoreboard =
-        ScoreboardStatus.isPublicScoreboard()
-            || (Validate.validateSession(ses)
-                && ScoreboardStatus.canSeeScoreboard((String) ses.getAttribute("userRole")));
-    if (canSeeScoreboard) {
+    if (ScoreboardStatus.canSeeScoreboard((String) ses.getAttribute("userRole"))) {
       out.write("true");
     } else {
       // Return 403
